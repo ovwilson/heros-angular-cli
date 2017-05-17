3fb06d0f2744a5961ceb97506ab2f8611077103b
landough <orlando_wilson@msn.com>
2017-05-17 07:57:12 -0400

    sort updates

--- src/app/directives/sort.directive.ts
+++ src/app/directives/sort.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, ElementRef, Renderer2, HostBinding, HostListener } from '@angular/core';
+import { Directive, Input, Output, EventEmitter, ElementRef, Renderer2, HostBinding, HostListener } from '@angular/core';
 
 const upIcon = 'arrow_upward';
 const downIcon = 'arrow_downward';
@@ -8,24 +8,40 @@ const downIcon = 'arrow_downward';
 })
 export class SortDirective {
 
-  @Input() sortArray: Array<any[]>;
+  @Input() name: string;
+  @Output() onSort = new EventEmitter();
 
   @HostListener('click') onClick() {
-    this.resetSorts(this.el);
-    this.toggleDirection(this.el);
+    this.toggleDirection();
+    this.sendSort();
   }
 
-  resetSorts(element: ElementRef){
-    
-  }
-
-  toggleDirection(element: ElementRef) {
-    const mdIconElement = element.nativeElement.querySelector('md-icon');
-    const html = mdIconElement.innerHTML;
-    switch (html) {
+  toggleDirection() {
+    const mdIconElement = this.el.nativeElement.querySelector('md-icon');
+    const direction = mdIconElement.innerHTML;
+    this.resetSorts();
+    switch (direction) {
       case 'arrow_upward': this.renderer.setProperty(mdIconElement, 'innerHTML', downIcon); break;
       case 'arrow_downward': this.renderer.setProperty(mdIconElement, 'innerHTML', upIcon); break;
       default: this.renderer.setProperty(mdIconElement, 'innerHTML', upIcon); break;
+    }
+  }
+
+  resetSorts() {
+    const tr = this.el.nativeElement.parentElement.parentElement;
+    const ths = tr.querySelectorAll('th');
+    for (let th of ths) {
+      const icon = th.querySelector('md-icon');
+      this.renderer.setProperty(icon, 'innerHTML', '');
+    }
+  }
+
+  sendSort() {
+    const direction = this.el.nativeElement.querySelector('md-icon').innerHTML;
+    switch (direction) {
+      case 'arrow_upward': this.onSort.emit({ name: name, direction: 'asc' }); break;
+      case 'arrow_downward': this.onSort.emit({ name: name, direction: 'dsc' }); break;
+      default: break;
     }
   }
 
